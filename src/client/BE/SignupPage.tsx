41d41ab13ca747889bf46d1b9b203cf07aea01a1
ChongWeiJie29 <chongweijie29@gmail.com>
2023-05-27 11:45:44 +0800

    Task 3: Handle login and sign up with BE Server and DB

--- src/client/BE/SignupPage.tsx
+++ src/client/BE/SignupPage.tsx
@@ -1,5 +1,12 @@
-export const handleSignup = async (username, password, navigation) => {
-    // Send login request and validate credentials on the server-side
-    // Replace this with your own implementation
+import axios from "axios";
+import {PORT} from '@env';
 
+export const handleSignup = async (username, password, navigation) => {
+    const resp = await axios.post(`http://10.0.2.2:${PORT}/api/signup`, {username, password});
+    if (resp.data.error) {
+        alert(resp.data.error);
+        return;
+    }
+    alert(resp.data.data);
+    navigation.navigate("Login");
 }
