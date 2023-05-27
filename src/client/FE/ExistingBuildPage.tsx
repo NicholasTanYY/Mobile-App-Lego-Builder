724a83e1acbaec14d22a2fcab134f626fa6f7213
Nicholas <nicholastanyunyu@gmail.com>
2023-05-27 20:04:12 +0800

    Task4: Modify new Build Page

--- src/client/FE/ExistingBuildPage.tsx
+++ src/client/FE/ExistingBuildPage.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import TextComponent from '../Assets/TextComponent';
 
-const ExistingBuildPage = () => {
+const ExistingBuildPage = ({ route, navigation }) => {
+  const { selectedLegoSet } = route.params;
 
   return (
     <View style={styles.container}>
-        <TextComponent type="textTitle" text="Existing Project Screen"/>
+      <TextComponent type="textTitle" text="Existing Project Screen" />
     </View>
   );
 };
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
-
 
 export default ExistingBuildPage;
