fb80565b192239907fd943d77c0cce070b849c4a
Nicholas <nicholastanyunyu@gmail.com>
2023-05-28 18:11:08 +0800

    Task6: Populated FAQ Page

--- src/client/data/FAQdata.tsx
+++ src/client/data/FAQdata.tsx
@@ -1,18 +1,37 @@
 export const FAQquestions = [
-    {
-      id: 1,
-      question: 'Question 1',
-      description: 'Answer to question 1',
-    },
-    {
-      id: 2,
-      question: 'Question 2',
-      description: 'Answer to question 2',
-    },
-    {
-      id: 3,
-      question: 'Question 3',
-      description: 'Answer to question 3',
-    },
-    // Add more questions and answers as needed
-  ];
+  {
+    id: 1,
+    question: 'What is BuildSquad?',
+    description:
+      'BuildSquad is a mobile application built by a group of NUS students who are interested in Legos. \
+      The application helps you simplify the process of building your Legos so that you can focus on the hands-on beauty of building your Lego.',
+  },
+  {
+    id: 2,
+    question: 'What are some features of this application?',
+    description:
+      'This application is a comprehensive tool for Lego enthusiasts. It includes a model recognition \
+      feature, a build planner, and a personal Lego set database. It even provides access to official Lego building instructions!',
+  },
+  {
+    id: 3,
+    question: 'What is the build planner? How do I use it?',
+    description:
+      'The build planner is a visual guide that helps you construct Lego models. It shows you step-by-step \
+    instructions based on the Lego bricks you have. Just input the Lego bricks in your possession, and the build planner will do the rest!',
+  },
+  {
+    id: 4,
+    question: 'How does the model recognition feature work?',
+    description:
+      'Our model recognition feature uses state-of-the-art machine learning algorithms. Simply provide an \
+    image of a Lego brick, and our system will identify the model and color of the brick.',
+  },
+  {
+    id: 5,
+    question: 'How is my data stored and used?',
+    description:
+      'We respect your privacy. Your data is securely stored and will only be used to provide the services \
+    within the app. We do not share your data with third parties without your explicit consent.',
+  },
+];
