391b61876721dc7d7224e890ccb8c2c549d0ba8f
Nicholas <nicholastanyunyu@gmail.com>
2023-07-23 15:38:29 +0800

    Task25: Added UI components into files

---
+++ src/client/Assets/Backgrounds.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, ImageBackground, StyleSheet } from 'react-native';
+
+export const LegoBorderBackground = ({ props }) => {
+  return (
+    <ImageBackground
+      source={require('./Images/lego_border.jpg')}
+      style={styles.background}>
+      {props.children}
+      {/* <View style={styles.container}>{children}</View> */}
+    </ImageBackground>
+  );
+};
+
+export const WhiteBackground = ({ props }) => {
+  return (
+    <ImageBackground
+      source={require('./Images/white_lego_background.jpg')}
+      style={styles.background}>
+      {props.children}
+      {/* <View style={styles.container}>{children}</View> */}
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+});
